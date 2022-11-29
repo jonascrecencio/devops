1cc5d9661fd933dd23d00b1e1c8a94e39a7201ab
Jonas Crescencio <jonas.crescencio@jonas.stone.local>
2022-11-29 15:43:21 -0300

    teste

--- netsnmp/run.py
+++ netsnmp/run.py
@@ -1,5 +1,12 @@
 from argparse import ArgumentParser
 from netsnmp.walker import cbqosWalker
+from prometheus_client import start_http_server
+from sys import stderr, stdout
+import time
+import gevent
+
+PROMETHEUS_CLIENT_PORT=9105
+SNMP_WALK_INTERVAL=30
 
 def parse_args():
     parser = ArgumentParser(prog="net-snmp-cbqos", description="SNMP cbQos exporter")
@@ -13,8 +20,27 @@ def parse_args():
 
 def run():
     args = parse_args()
+    start_http_server(PROMETHEUS_CLIENT_PORT)
     walker = cbqosWalker(args)
-    qos_rates = walker.walk_snmp()
-    walker.prom_export(qos_rates)
-    #interfaces = walker.walk_interfaces()
-    #qos_interfaces = walker.check_qos_policy(interfaces)
+    metric = walker.define_metric()
+
+    while True:        
+        try:
+            start = time.time()
+            qos_if_rates = walker.walk_snmp()
+            spawns = []
+            for rate in qos_if_rates:
+                spawn = gevent.spawn(
+                    walker.monit_rate,
+                    rate=rate,
+                    metric=metric
+                )
+                spawns.append(spawn)   
+            stdout.flush()
+            stderr.flush()
+            end = time.time()
+        except Exception:
+            pass
+        time_remaining = SNMP_WALK_INTERVAL - (end - start)
+        wait = time_remaining if time_remaining > 0 else 0
+        gevent.sleep(wait)
--- netsnmp/walker.py
+++ netsnmp/walker.py
@@ -1,9 +1,8 @@
-from prometheus_client import Gauge, start_http_server
+from prometheus_client import Gauge
 from easysnmp import Session
 
-PROMETHEUS_CLIENT_PORT="9105"
 INTERFACE_NAME_OID="1.3.6.1.2.1.2.2.1.2"
-INTERFACE_INDEX_OID="1.3.6.1.2.1.2.2.1.1"
+#INTERFACE_INDEX_OID="1.3.6.1.2.1.2.2.1.1"
 INTERFACE_CBQOS_INDEX="1.3.6.1.4.1.9.9.166.1.1.1.1.4"
 CBQOS_OID="1.3.6.1.4.1.9.9.166"
 CBQOS_POLICY_DIRECTION="1.3.6.1.4.1.9.9.166.1.1.1.1.3"
@@ -11,9 +10,6 @@ CBQOS_CLASSES="1.3.6.1.4.1.9.9.166.1.7.1.1.1"
 CBQOS_OBJECTS="1.3.6.1.4.1.9.9.166.1.5.1.1.2"
 CBQOS_TYPE="1.3.6.1.4.1.9.9.166.1.5.1.1.3"
 CBQOS_BIT_RATE="1.3.6.1.4.1.9.9.166.1.15.1.1.11"
-
-#1.3.6.1.2.1.2.2.1.2 (nome)
-#1.3.6.1.2.1.2.2.1.1 (index) o que interessa por baixo dos panos
 
 class cbqosWalker():
     def __init__(self, args):
@@ -23,7 +19,7 @@ class cbqosWalker():
 
     def walk_snmp(self):
         session = Session(hostname=self.address, community=self.community, version=2)
-        if_indexes = session.bulkwalk(INTERFACE_INDEX_OID)
+        #if_indexes = session.bulkwalk(INTERFACE_INDEX_OID)
         if_names = session.bulkwalk(INTERFACE_NAME_OID)
         if_cbqos = session.bulkwalk(INTERFACE_CBQOS_INDEX)
         cbqos_pol_direction = session.bulkwalk(CBQOS_POLICY_DIRECTION)
@@ -46,47 +42,24 @@ class cbqosWalker():
                 "rate" : rate,
                 "policy" : policy,
                 "direction" : direction,
-                "class" : cbqos_class,
+                "qos_class" : cbqos_class,
                 "interface" : interface
             })
-        print('########################### if_rates OK')
         return if_rates
 
-    def prom_export(self, rates):
-        print('########################## start http server')
-        start_http_server(PROMETHEUS_CLIENT_PORT)
-        print('#################################33 create metric')
+    def define_metric(self):
         metric = Gauge(
             "cbQosCMPostPolicyBitRate",
             "The bit rate of the traffic after executing QoS policies.",
             ['interface', 'direction', 'qos_class', 'policy']
         )
-        print('######################### metric ok')
-        for rate in rates:
-            prometheus_metric_labels = {
-                "interface" : rate['interface'],
-                "direction" : rate['direction'],
-                "qos_class" : rate['qos_class'],
-                "policy" : rate['policy']
-            }
-            print('############ metric', prometheus_metric_labels)
-            metric.labels(**prometheus_metric_labels).set(rate['rate'])
-            print('########### set metric ok')
-        print('Done.')
-# def init_metric():
-#     class_rate = Gauge(
-#         "cbQosCMPostPolicyBitRate",
-#         "The bit rate of the traffic after executing QoS policies.",
-#         ['interface', 'direction', 'class']
-#     )
-#     return class_rate
+        return metric
 
-# def run():
-#     prometheus_metric_labels = {
-#         "interface": "GigaEthernet 4",
-#         "direction": "in",
-#         "class": "ouro-pix",
-#     }
-#     start_http_server(PROMETHEUS_CLIENT_PORT)
-#     metric = init_metric()
-#     metric.labels(**prometheus_metric_labels).set(300000)
+    def monit_rate(self, rate, metric):
+        prometheus_metric_labels = {
+            "interface" : rate['interface'],
+            "direction" : rate['direction'],
+            "qos_class" : rate['qos_class'],
+            "policy" : rate['policy']
+        }
+        metric.labels(**prometheus_metric_labels).set(rate['rate'])
